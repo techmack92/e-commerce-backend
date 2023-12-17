4a9777211372cc166113b9a9f69404f18b5ff904
Mack <mldixon9750@gmail.com>
2023-12-16 19:31:30 -0800

    updated models Category, Product, Tag and ProductTag

--- models/Category.js
+++ models/Category.js
@@ -6,10 +6,21 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
+    // Link to database connection
     sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
     timestamps: false,
     freezeTableName: true,
     underscored: true,
